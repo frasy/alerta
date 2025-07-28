8cc75222c7d85b52fcdf18f783ada8f1139b27bf
Francisco Fernandez <franciscofe@tekn.group>
2025-07-28 13:19:58 +0000

    (feat) Add pagination support to environments endpoint #1665

--- alerta/database/backends/mongodb/base.py
+++ alerta/database/backends/mongodb/base.py
@@ -797,7 +797,7 @@ class Backend(Database):
 
     # ENVIRONMENTS
 
-    def get_environments(self, query=None, topn=1000):
+    def get_environments(self, query=None, page=1, page_size=1000):
         query = query or Query()
 
         def pipeline(group_by):
@@ -809,8 +809,7 @@ class Backend(Database):
                      '_id': {'environment': '$environment', group_by: '$' + group_by},
                      'count': {'$sum': 1}
                  }
-                 },
-                {'$limit': topn}
+                }
             ]
 
         response_severity = self.get_db().alerts.aggregate(pipeline('severity'))
@@ -823,14 +822,30 @@ class Backend(Database):
         for r in response_status:
             status_count[r['_id']['environment']].append((r['_id']['status'], r['count']))
 
-        environments = self.get_db().alerts.find().distinct('environment')
+        pipeline_different_envs = [
+            {'$group':{
+                    '_id': '$environment',
+                }
+            },
+            {'$sort': {'_id': 1}},
+            {'$skip': (page - 1) * page_size},
+            {'$limit': page_size}
+        ]
+        environments = self.get_db().alerts.aggregate(pipeline_different_envs)
         return [
             {
-                'environment': env,
-                'severityCounts': dict(severity_count[env]),
-                'statusCounts': dict(status_count[env]),
-                'count': sum(t[1] for t in severity_count[env])
+                'environment': env['_id'],
+                'severityCounts': dict(severity_count[env['_id']]),
+                'statusCounts': dict(status_count[env['_id']]),
+                'count': sum(t[1] for t in severity_count[env['_id']])
             } for env in environments]
+    
+    def get_environments_count(self, query=None):
+        query = query or Query()
+        where = query.where
+        distinct_values = self.get_db().alerts.distinct("environment", where)
+        count = len(distinct_values) if distinct_values else 0
+        return count
 
     # SERVICES
 
